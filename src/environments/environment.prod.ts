e5a3a0d870eb5a8ecc0d2a1e0a837b7a6c18f6cb
Jesus Ventura <javentura@salud.gob.sv>
2019-06-24 08:51:56 -0600

    Se envia codigo AppNoticiasIonci a github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '366f51d819ae4f67a059f874fffbfa03',
+  apiUrl: 'https://newsapi.org/v2'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '366f51d819ae4f67a059f874fffbfa03',
+  apiUrl: 'https://newsapi.org/v2'
 };
 
 /*
