b2f1ae28f4c224550c38c2a6920f92118cafea18
Jesus Ventura <javentura@salud.gob.sv>
2019-07-12 13:24:04 -0600

    Se convierte la app a pwa, fin ejercicio

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, Platform } from '@ionic/angular';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { DataLocalService } from '../../services/data-local.service';
 
@@ -21,7 +21,8 @@ export class NoticiaComponent implements OnInit {
   constructor( private iab: InAppBrowser,
                private actionSheetCtrl: ActionSheetController,
                private socialSharing: SocialSharing,
-               private datalocalService: DataLocalService) { }
+               private datalocalService: DataLocalService,
+               private platform: Platform) { }
 
   ngOnInit() {}
 
@@ -64,13 +65,7 @@ export class NoticiaComponent implements OnInit {
         icon: 'share',
         cssClass: 'action-dark',
         handler: () => {
-          console.log('Share clicked');
-          this.socialSharing.share(
-            this.noticia.title,
-            this.noticia.source.name,
-            '',
-            this.noticia.url
-          );
+          this.compartirNoticia();
         }
       },
       guardarBorrarBtn,
@@ -86,6 +81,29 @@ export class NoticiaComponent implements OnInit {
     });
     await actionSheet.present();
 
+  }
+
+  compartirNoticia() {
+    if (this.platform.is('cordova')) {
+      this.socialSharing.share(
+        this.noticia.title,
+        this.noticia.source.name,
+        '',
+        this.noticia.url
+      );
+    } else {
+      if (navigator['share']) {
+        navigator['share']({
+            title: this.noticia.title,
+            text: this.noticia.description,
+            url: 'https://developers.google.com/web',
+        })
+          .then(() => console.log('Successful share'))
+          .catch((error) => console.log('Error sharing', error));
+      } else {
+        console.log("No se pudo compartir");
+      }
+    }
   }
 
 }
